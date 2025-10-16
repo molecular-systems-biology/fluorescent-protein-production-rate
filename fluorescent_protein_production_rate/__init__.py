33615ef57ccb6e4decf6d9bf7e72b0c8959bb11b
Andrezylstra <andre.zylstra@gmail.com>
2025-10-16 11:58:32 +0200

    Update init

--- fluorescent_protein_production_rate/__init__.py
+++ fluorescent_protein_production_rate/__init__.py
@@ -1,11 +1,13 @@
 from .fluorescent_protein_production_rate import (
     MotherCellCycle,
+    DaughterCellCycle,
     FluorescentProteinProductionRateExperiment,
     get_version
 )
 
 __all__ = [
-    "MotherCellCycle", 
+    "MotherCellCycle",
+    "DaughterCellCycle",
     "FluorescentProteinProductionRateExperiment", 
     "get_version"
 ]
